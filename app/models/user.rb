97298b61518b319df03d42d529716e8722a1b5c3
riffiy <117071448+riffiy@users.noreply.github.com>
2022-12-02 11:06:55 +0000

    [add]debug2いいね機能実装中2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :books
+  
   has_many :favorites, dependent: :destroy
+  has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
  
   has_one_attached :profile_image
